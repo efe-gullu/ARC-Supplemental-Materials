95c0cbb1c0c82a01bd6dd3c9d3164be7e054e20a
efe-gullu <114932298+efe-gullu@users.noreply.github.com>
2025-06-10 00:31:01 +0300

    Update ArmSubsystem.java

--- Task2/src/ArmSubsystem.java
+++ Task2/src/ArmSubsystem.java
@@ -7,41 +7,33 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+public class ExampleSubsystem extends SubsystemBase {  
+  
+  public ArmSubsystem() {}
+  
+  private final TalonFX arm = new TalonFX(20); 
+  private final double gearRatio = 10.0;    //given reduction ratio
+  private double currentRotations = 0.0;
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  
+  private double degreesToRotations(double degrees) { // method to convert degrees to motor rotations
+      return degrees / 360.0 * gearRatio;
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  
+  private double rotationsToDegrees(double rotations) { // method to converts motor rotations  to degrees
+      return rotations / gearRatio * 360.0;
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+
+  public void setAngle(double degrees) {    //method the set the desired angle given in degrees
+      currentRotations = degreesToRotations(degrees);          //rotations from the given degree
+      System.out.println("Setting arm to " + degrees + " degrees (" + currentRotations + " motor rotations)");
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+ 
+  public double getAngle() {      // method to get the current arm angle given in degrees
+      double angle = rotationsToDegrees(currentRotations);     //angle from the rotation
+      return angle;
   }
 }
